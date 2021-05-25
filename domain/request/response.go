445f8ae680fe18f98f5801ae29e9f18304d96ce2
Felix Seemann <felix.seemann@code-fabrik.com>
2021-05-25 12:32:17 +0200

    Fix json name

--- domain/request/response.go
+++ domain/request/response.go
@@ -4,6 +4,6 @@ type Response struct {
 	Error              string              `json:"error,omitempty"`
 	Target             string              `json:"target,omitempty"`
 	ResponseBody       string              `json:"responseBody,omitempty"`
-	ResponseHeader     map[string][]string `json:"header,omitempty"`
+	ResponseHeader     map[string][]string `json:"responseHeader,omitempty"`
 	ResponseStatusCode int                 `json:"responseStatusCode,omitempty"`
 }
